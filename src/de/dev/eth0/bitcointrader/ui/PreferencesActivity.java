621a69ff20fea9b48271950d829adc44beecf744
deveth0 <deveth0@46fdfbd1-27fd-a4d1-f970-16196c59cddc>
2013-06-16 13:05:57 +0000

    Bitcointrader: Updated Preferences
    git-svn-id: http://bitcointrader.googlecode.com/svn/Code/Privat/Android/BitcoinTrader/BitcoinTrader@15 46fdfbd1-27fd-a4d1-f970-16196c59cddc

--- src/de/dev/eth0/bitcointrader/ui/PreferencesActivity.java
+++ src/de/dev/eth0/bitcointrader/ui/PreferencesActivity.java
@@ -4,8 +4,9 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
-import android.provider.SyncStateContract;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.text.TextUtils;
 
 import com.actionbarsherlock.app.ActionBar;
@@ -29,14 +30,30 @@ public final class PreferencesActivity extends SherlockPreferenceActivity implem
     sp.registerOnSharedPreferenceChangeListener(this);
 
     //@TODO remove mtgox settings if not activated
-    
+
     // if api key has not been set yet, add the summary, otherwise the current value
-    String mtgoxApiKey = sp.getString(Constants.PREFS_KEY_MTGOX_APIKEY, null);
+    String mtgoxApiKey = sp.getString(Constants.PREFS_KEY_MTGOX_ACTIVATIONKEY, null);
     if (TextUtils.isEmpty(mtgoxApiKey)) {
-      findPreference(Constants.PREFS_KEY_MTGOX_APIKEY).setSummary(R.string.preferences_mtgox_api_key_summary);
-    } else {
-      findPreference(Constants.PREFS_KEY_MTGOX_APIKEY).setSummary(mtgoxApiKey);
+      findPreference(Constants.PREFS_KEY_MTGOX_ACTIVATIONKEY).setSummary(R.string.preferences_mtgox_activation_key_summary);
     }
+    else {
+      findPreference(Constants.PREFS_KEY_MTGOX_ACTIVATIONKEY).setSummary(mtgoxApiKey);
+    }
+
+    final ListPreference listPreference = (ListPreference)findPreference(Constants.PREFS_KEY_GENERAL_UPDATE);
+    if (listPreference.getValue() == null) {
+      listPreference.setValueIndex(0);
+    }
+    listPreference.setSummary(listPreference.getEntry());
+    listPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+      @Override
+      public boolean onPreferenceChange(Preference preference, Object newValue) {
+        listPreference.setValue(newValue.toString());
+        // Get the entry which corresponds to the current value and set as summary
+        preference.setSummary(listPreference.getEntry());
+        return false;
+      }
+    });
   }
 
   @Override
@@ -51,12 +68,13 @@ public final class PreferencesActivity extends SherlockPreferenceActivity implem
 
   public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
     // Update summary for api key changed
-    if (TextUtils.equals(key, Constants.PREFS_KEY_MTGOX_APIKEY)) {
+    if (TextUtils.equals(key, Constants.PREFS_KEY_MTGOX_ACTIVATIONKEY)) {
       Preference pref = findPreference(key);
-      EditTextPreference etp = (EditTextPreference) pref;
+      EditTextPreference etp = (EditTextPreference)pref;
       if (TextUtils.isEmpty(etp.getText())) {
-        pref.setSummary(R.string.preferences_mtgox_api_key_summary);
-      } else {
+        pref.setSummary(R.string.preferences_mtgox_activation_key_summary);
+      }
+      else {
         pref.setSummary(etp.getText());
       }
     }
