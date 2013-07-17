c35a2689e3994ec9815ee5c636b638fdfbfda676
deveth0 <deveth0@46fdfbd1-27fd-a4d1-f970-16196c59cddc>
2013-07-17 14:24:50 +0000

    Bitcointrader: refs #40 (Version of Price Chart Graphs)
    git-svn-id: http://bitcointrader.googlecode.com/svn/Code/Privat/Android/BitcoinTrader/BitcoinTrader@172 46fdfbd1-27fd-a4d1-f970-16196c59cddc

--- src/de/dev/eth0/bitcointrader/ui/fragments/PriceChartDetailFragment.java
+++ src/de/dev/eth0/bitcointrader/ui/fragments/PriceChartDetailFragment.java
@@ -3,23 +3,31 @@
 package de.dev.eth0.bitcointrader.ui.fragments;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
-import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.BarGraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.LineGraphView;
+import com.jjoe64.graphview.GraphViewStyle;
+import com.xeiam.xchange.bitcoincharts.BitcoinChartsAdapters;
+import com.xeiam.xchange.bitcoincharts.BitcoinChartsFactory;
+import com.xeiam.xchange.bitcoincharts.dto.charts.ChartData;
 import de.dev.eth0.bitcointrader.R;
 import de.dev.eth0.bitcointrader.BitcoinTraderApplication;
+import de.dev.eth0.bitcointrader.Constants;
 import de.dev.eth0.bitcointrader.ui.AbstractBitcoinTraderActivity;
-import de.dev.eth0.bitcointrader.ui.PriceChartActivity;
+import de.dev.eth0.bitcointrader.ui.PriceChartDetailActivity;
+import de.dev.eth0.bitcointrader.util.ICSAsyncTask;
 import de.schildbach.wallet.ui.HelpDialogFragment;
 
 /**
@@ -30,6 +38,9 @@ public class PriceChartDetailFragment extends AbstractBitcoinTraderFragment {
   private static final String TAG = PriceChartDetailFragment.class.getSimpleName();
   private BitcoinTraderApplication application;
   private AbstractBitcoinTraderActivity activity;
+  private ProgressDialog mDialog;
+  private BarGraphView graphView;
+  private GraphViewSeries graphViewSeries;
 
   @Override
   public void onAttach(Activity activity) {
@@ -68,38 +79,99 @@ public class PriceChartDetailFragment extends AbstractBitcoinTraderFragment {
   @Override
   public void onViewCreated(View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    // draw sin curve
-    int num = 150;
-    GraphView.GraphViewData[] data = new GraphView.GraphViewData[num];
-    double v = 0;
-    for (int i = 0; i < num; i++) {
-      v += 0.2;
-      data[i] = new GraphView.GraphViewData(i, Math.sin(v));
-    }
-    // graph with dynamically genereated horizontal and vertical labels
-    GraphView graphView;
-    graphView = new LineGraphView(activity, "GraphViewDemo");
-
-    // add data
-    graphView.addSeries(new GraphViewSeries(data));
-    // set view port, start=2, size=40
-    graphView.setViewPort(2, 40);
+    graphView = new BarGraphView(activity, "GraphViewDemo");
     graphView.setScrollable(true);
+    graphView.setGraphViewStyle(new GraphViewStyle(Color.BLACK, Color.BLACK, Color.WHITE));
     LinearLayout layout = (LinearLayout)view.findViewById(R.id.price_chart_detail_graph);
     layout.addView(graphView);
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (mDialog != null && mDialog.isShowing()) {
+      mDialog.dismiss();
+    }
+    mDialog = null;
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    updateView();
   }
 
   @Override
   public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
     super.onCreateOptionsMenu(menu, inflater);
     // only add menu if included in pricechartactivity
-    if (activity instanceof PriceChartActivity) {
+    if (activity instanceof PriceChartDetailActivity) {
       inflater.inflate(R.menu.pricechart_options, menu);
     }
   }
 
   protected void updateView() {
     Log.d(TAG, ".updateView()");
-
+    PriceChartDetailFragment.GetChartDataTask tradesTask = new PriceChartDetailFragment.GetChartDataTask();
+    tradesTask.executeOnExecutor(ICSAsyncTask.SERIAL_EXECUTOR);
   }
+
+  protected void updateView(ChartData[] chartData) {
+    Log.d(TAG, ".updateView");
+    if (chartData != null) {
+      GraphViewData[] data = new GraphViewData[chartData.length];
+
+      for (int i = 0; i < chartData.length; i++) {
+        ChartData cd = chartData[i];
+        Log.d(TAG, "new entry: " + cd.toString());
+        data[i] = new GraphViewData(i, cd.getWeightedPrice().doubleValue());
+        Log.d(TAG, "entry: " + data[i].valueX + "/" + data[i].valueY);
+      }
+      if (graphViewSeries == null) {
+        graphViewSeries = new GraphViewSeries(data);
+        graphView.addSeries(graphViewSeries);
+      }
+      else {
+        graphViewSeries.resetData(data);
+      }
+      graphView.redrawAll();
+    }
+  }
+
+  private class GetChartDataTask extends ICSAsyncTask<Void, Void, ChartData[]> {
+
+    @Override
+    protected void onPreExecute() {
+      if (mDialog == null) {
+        mDialog = new ProgressDialog(activity);
+        mDialog.setMessage(getString(R.string.loading_info));
+        mDialog.setCancelable(false);
+        mDialog.setOwnerActivity(activity);
+        mDialog.show();
+      }
+    }
+
+    @Override
+    protected void onPostExecute(ChartData[] chartData) {
+      if (mDialog != null && mDialog.isShowing()) {
+        mDialog.dismiss();
+        mDialog = null;
+      }
+      Log.d(TAG, "Found " + (chartData != null ? chartData.length : 0) + " ticker entries");
+      updateView(chartData);
+    }
+
+    @Override
+    protected ChartData[] doInBackground(Void... params) {
+      try {
+        ChartData[] chartdata = BitcoinChartsAdapters.adaptChartData(BitcoinChartsFactory.createInstance().getChartData("mtGoxUSD", 1));
+        return chartdata == null ? new ChartData[0] : chartdata;
+      }
+     catch (Exception e) {
+        activity.sendBroadcast(new Intent(Constants.UPDATE_FAILED));
+        Log.e(TAG, "Exception", e);
+      }
+      return null;
+    }
+  };
 }
