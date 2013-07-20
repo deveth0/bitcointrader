352f8798d6528b9ee40e73f69cd16e89ba6ddd2d
deveth0 <deveth0@46fdfbd1-27fd-a4d1-f970-16196c59cddc>
2013-07-20 22:16:48 +0000

    Bitcointrader: refs #40 (reverted changes in trunk)
    git-svn-id: http://bitcointrader.googlecode.com/svn/Code/Privat/Android/BitcoinTrader/BitcoinTrader@179 46fdfbd1-27fd-a4d1-f970-16196c59cddc

--- src/de/dev/eth0/bitcointrader/ui/PriceChartDetailActivity.java
+++ src/de/dev/eth0/bitcointrader/ui/PriceChartDetailActivity.java
@@ -2,39 +2,22 @@
 //$Id$
 package de.dev.eth0.bitcointrader.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
+import com.actionbarsherlock.app.ActionBar;
+
+import com.actionbarsherlock.view.MenuItem;
 import de.dev.eth0.bitcointrader.R;
-import de.dev.eth0.bitcointrader.ui.fragments.PriceChartDetailFragment;
 /**
  * @author Alexander Muthmann
  */
 public class PriceChartDetailActivity extends AbstractBitcoinTraderActivity {
 
-  public final static String INTENT_EXTRA_EXCHANGE = "exchange";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.price_chart_detail_activity);
-    handleIntent(getIntent());
   }
 
-  @Override
-  protected void onNewIntent(Intent intent) {
-    handleIntent(intent);
-  }
-
-  private void handleIntent(Intent intent) {
-    if (intent.hasExtra(INTENT_EXTRA_EXCHANGE)) {
-      String exchange = intent.getStringExtra(INTENT_EXTRA_EXCHANGE);
-      updatePriceChartDetailFragment(exchange);
-    }
-  }
-
-  private void updatePriceChartDetailFragment(String exchange) {
-    PriceChartDetailFragment priceChartDetailFragment = (PriceChartDetailFragment)getSupportFragmentManager().findFragmentById(R.id.price_chart_detail_fragment);
-    priceChartDetailFragment.update(exchange);
-  }
 }
--- src/de/dev/eth0/bitcointrader/ui/fragments/PriceChartDetailFragment.java
+++ src/de/dev/eth0/bitcointrader/ui/fragments/PriceChartDetailFragment.java
@@ -3,32 +3,20 @@
 package de.dev.eth0.bitcointrader.ui.fragments;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.GraphViewStyle;
-import com.jjoe64.graphview.LineGraphView;
-import com.xeiam.xchange.bitcoincharts.BitcoinChartsAdapters;
-import com.xeiam.xchange.bitcoincharts.BitcoinChartsFactory;
-import com.xeiam.xchange.bitcoincharts.dto.charts.ChartData;
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 import de.dev.eth0.bitcointrader.R;
 import de.dev.eth0.bitcointrader.BitcoinTraderApplication;
-import de.dev.eth0.bitcointrader.Constants;
 import de.dev.eth0.bitcointrader.ui.AbstractBitcoinTraderActivity;
-import de.dev.eth0.bitcointrader.ui.PriceChartDetailActivity;
-import de.dev.eth0.bitcointrader.util.ICSAsyncTask;
+import de.dev.eth0.bitcointrader.ui.PriceChartActivity;
 import de.schildbach.wallet.ui.HelpDialogFragment;
 
 /**
@@ -39,10 +27,7 @@ public class PriceChartDetailFragment extends AbstractBitcoinTraderFragment {
   private static final String TAG = PriceChartDetailFragment.class.getSimpleName();
   private BitcoinTraderApplication application;
   private AbstractBitcoinTraderActivity activity;
-  private ProgressDialog mDialog;
-  private GraphView graphView;
-  private GraphViewSeries graphViewSeries;
-  private String exchange;
+  private ImageView imageView;
 
   @Override
   public void onAttach(Activity activity) {
@@ -81,109 +66,23 @@ public class PriceChartDetailFragment extends AbstractBitcoinTraderFragment {
   @Override
   public void onViewCreated(View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    graphView = new LineGraphView(activity, "GraphViewDemo");
-    graphView.setScrollable(true);
-    graphView.setGraphViewStyle(new GraphViewStyle(Color.BLACK, Color.BLACK, Color.WHITE));
-    LinearLayout layout = (LinearLayout)view.findViewById(R.id.price_chart_detail_graph);
-    layout.addView(graphView);
-  }
-
-  @Override
-  public void onPause() {
-    super.onPause();
-    if (mDialog != null && mDialog.isShowing()) {
-      mDialog.dismiss();
-    }
-    mDialog = null;
-  }
-
-  @Override
-  public void onResume() {
-    super.onResume();
-    updateView();
+    imageView = (ImageView)view.findViewById(R.id.price_chart_detail_image);
+    UrlImageViewHelper.setUrlDrawable(imageView,
+            "http://bitcoincharts.com/charts/chart.png?m=mtgoxUSD&v=1&t=S&noheader=1&height=80&width=750&r=60",
+            R.drawable.loading);
   }
 
   @Override
   public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
     super.onCreateOptionsMenu(menu, inflater);
     // only add menu if included in pricechartactivity
-    if (activity instanceof PriceChartDetailActivity) {
+    if (activity instanceof PriceChartActivity) {
       inflater.inflate(R.menu.pricechart_options, menu);
     }
   }
 
   protected void updateView() {
     Log.d(TAG, ".updateView()");
-    PriceChartDetailFragment.GetChartDataTask tradesTask = new PriceChartDetailFragment.GetChartDataTask();
-    tradesTask.executeOnExecutor(ICSAsyncTask.SERIAL_EXECUTOR);
+
   }
-
-  protected void updateView(ChartData[] chartData) {
-    Log.d(TAG, ".updateView");
-    if (chartData != null) {
-      GraphViewData[] data = new GraphViewData[chartData.length];
-      double min = Double.MAX_VALUE;
-      double max = Double.MIN_VALUE;
-      for (int i = 0; i < chartData.length; i++) {
-        ChartData cd = chartData[i];
-        Log.d(TAG, "new entry: " + cd.toString());
-        data[i] = new GraphViewData(i, cd.getWeightedPrice().doubleValue());
-        min = Math.min(data[i].valueY, min);
-        max = Math.max(data[i].valueY, max);
-        Log.d(TAG, "entry: " + data[i].valueX + "/" + data[i].valueY);
-      }
-      if (graphViewSeries == null) {
-        graphViewSeries = new GraphViewSeries(data);
-        graphView.addSeries(graphViewSeries);
-      }
-      else {
-        graphViewSeries.resetData(data);
-      }
-      graphView.setManualYAxisBounds(max * 1.05, min * 0.95);
-      graphView.redrawAll();
-    }
-  }
-
-  public void update(String exchange) {
-    this.exchange = exchange;
-    this.graphView.setTitle(exchange);
-    updateView();
-  }
-
-  private class GetChartDataTask extends ICSAsyncTask<Void, Void, ChartData[]> {
-
-    @Override
-    protected void onPreExecute() {
-      if (mDialog == null) {
-        mDialog = new ProgressDialog(activity);
-        mDialog.setMessage(getString(R.string.loading_info));
-        mDialog.setCancelable(false);
-        mDialog.setOwnerActivity(activity);
-        mDialog.show();
-      }
-    }
-
-    @Override
-    protected void onPostExecute(ChartData[] chartData) {
-      if (mDialog != null && mDialog.isShowing()) {
-        mDialog.dismiss();
-        mDialog = null;
-      }
-      Log.d(TAG, "Found " + (chartData != null ? chartData.length : 0) + " ticker entries");
-      updateView(chartData);
-    }
-
-    @Override
-    protected ChartData[] doInBackground(Void... params) {
-      try {
-        ChartData[] chartdata = BitcoinChartsAdapters.adaptChartData(BitcoinChartsFactory.createInstance().getChartData(exchange, 1));
-        return chartdata == null ? new ChartData[0] : chartdata;
-      }
-      catch (Exception e) {
-        activity.sendBroadcast(new Intent(Constants.UPDATE_FAILED));
-        Log.e(TAG, "Exception", e);
-      }
-      return null;
-    }
-  };
 }
